49d8bb333b19319291d278c09b55425cb835c36a
DeveloperVusal <developer.vusal96@gmail.com>
2023-01-31 08:20:45 +0300

    feat(database): add feature helper getting of the .env variables

--- config/database.go
+++ config/database.go
@@ -1,31 +1,19 @@
 package config
 
 import (
-	"log"
-	"os"
-	"path/filepath"
-
-	"github.com/joho/godotenv"
+	"attrtour/core"
 )
 
 type DatabaseConfig struct{}
 
 func (dbcfg *DatabaseConfig) ConfigLoad() map[string]string {
-	dir, _ := os.Getwd()
-	projectPath := filepath.Join(filepath.Dir(dir), filepath.Base(dir))
-	filename := filepath.Join(projectPath, "/.env")
-
-	var err = godotenv.Load(filename)
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	env := core.Helpers{}
 
 	return map[string]string{
-		"host":     os.Getenv("DB_HOST"),
-		"port":     os.Getenv("DB_PORT"),
-		"username": os.Getenv("DB_USERNAME"),
-		"password": os.Getenv("DB_PASSWORD"),
-		"dbname":   os.Getenv("DB_DATABASE"),
+		"host":     env.Env("DB_HOST"),
+		"port":     env.Env("DB_PORT"),
+		"username": env.Env("DB_USERNAME"),
+		"password": env.Env("DB_PASSWORD"),
+		"dbname":   env.Env("DB_DATABASE"),
 	}
 }
